ade6e0575fe7b7ca018a55af4f31641d546c7ab0
gilzoide <gilzoide@gmail.com>
2020-07-17 15:11:24 -0300

    Add enum values to fswatch module, implicitly box fsw_event_type_filter on add_event_type_filter

--- example.lua
+++ example.lua
@@ -3,11 +3,15 @@ local fswatch = require 'fswatch'
 
 fswatch.set_verbose(arg[1] == '--verbose')
 
+local i = 0
+
 local session = fswatch.init_session()
 assert(session:set_callback(function(events)
     for path, time, flags in events:iter() do
         print(path, time, table.concat(flags, ' | '))
     end
+    i = i + 1
+    if i >= 5 then session:stop_monitor() end
 end))
 session:add_path(".")
 session:add_filter('.git', 'filter_exclude')
--- fswatch.lua
+++ fswatch.lua
@@ -1,24 +1,28 @@
 local ffi = require 'ffi'
 local libfswatch = require 'libfswatch'
 
+local FSW = {}
+
 local function create_enum(...)
     local enum = {}
     for i = 1, select('#', ...) do
-        local key = i - 1
-        local value = select(i, ...)
+        local key = select(i, ...)
+        local value = i - 1
+        FSW[key] = value
         enum[key] = value
         enum[value] = key
     end
     return enum
 end
 local function create_enum_options(indexed_zero, ...)
-    local enum = {
-        [0] = indexed_zero,
-        [indexed_zero] = 0,
-    }
+    local enum = {}
+    FSW[indexed_zero] = 0
+    enum[indexed_zero] = 0
+    enum[0] = indexed_zero
     for i = 1, select('#', ...) do
-        local key = bit.lshift(1, i - 1)
-        local value = select(i, ...)
+        local key = select(i, ...)
+        local value = bit.lshift(1, i - 1)
+        FSW[key] = value
         enum[key] = value
         enum[value] = key
     end
@@ -102,44 +106,54 @@ local function wrap_FSW_STATUS_returns(f)
     end
 end
 
+local function box_parameters_as_type(ctype, ...)
+    local obj = ...
+    if not ffi.istype(ctype, obj) then
+        return ctype(...)
+    else
+        return obj
+    end
+end
+
+local fsw_event_type_filter = ffi.typeof('fsw_event_type_filter')
 local fsw_cmonitor_filter = ffi.typeof('fsw_cmonitor_filter')
 
-local FSW = {
-    STATUS = FSW_STATUS,
-    event_flag = fsw_event_flag,
-    monitor_type = fsw_monitor_type,
+FSW.STATUS = FSW_STATUS
+FSW.event_flag = fsw_event_flag
+FSW.monitor_type = fsw_monitor_type
 
-    init_session = function(type)
-        type = type or fsw_monitor_type.system_default
-        return libfswatch.fsw_init_session(type)
-    end,
-    add_path = wrap_FSW_STATUS_returns(libfswatch.fsw_add_path),
-    add_property = wrap_FSW_STATUS_returns(libfswatch.fsw_add_property),
-    set_allow_overflow = wrap_FSW_STATUS_returns(libfswatch.fsw_set_allow_overflow),
-    set_callback = function(handle, callback)
-        local status = libfswatch.fsw_set_callback(handle, wrap_lua_callback(callback), nil)
-        return FSW_STATUS_to_lua_returns(status)
-    end,
-    set_latency = wrap_FSW_STATUS_returns(libfswatch.fsw_set_latency),
-    set_recursive = wrap_FSW_STATUS_returns(libfswatch.fsw_set_recursive),
-    set_directory_only = wrap_FSW_STATUS_returns(libfswatch.fsw_set_directory_only),
-    set_follow_symlinks = wrap_FSW_STATUS_returns(libfswatch.fsw_set_follow_symlinks),
-    add_event_type_filter = wrap_FSW_STATUS_returns(libfswatch.fsw_add_event_type_filter),
-    add_filter = function(session, cmonitor_filter, ...)
-        if not ffi.istype(fsw_cmonitor_filter, cmonitor_filter) and type(cmonitor_filter) ~= 'table' then
-            cmonitor_filter = fsw_cmonitor_filter(cmonitor_filter, ...)
-        end
-        local status = libfswatch.fsw_add_filter(session, cmonitor_filter)
-        return FSW_STATUS_to_lua_returns(status)
-    end,
-    start_monitor = wrap_FSW_STATUS_returns(libfswatch.fsw_start_monitor),
-    stop_monitor = wrap_FSW_STATUS_returns(libfswatch.fsw_stop_monitor),
-    is_running = libfswatch.fsw_is_running,
-    destroy_session = wrap_FSW_STATUS_returns(libfswatch.fsw_destroy_session),
-    last_error = libfswatch.fsw_last_error,
-    is_verbose = libfswatch.fsw_is_verbose,
-    set_verbose = libfswatch.fsw_set_verbose,
-}
+FSW.init_session = function(type)
+    type = type or fsw_monitor_type.system_default
+    return libfswatch.fsw_init_session(type)
+end
+FSW.add_path = wrap_FSW_STATUS_returns(libfswatch.fsw_add_path)
+FSW.add_property = wrap_FSW_STATUS_returns(libfswatch.fsw_add_property)
+FSW.set_allow_overflow = wrap_FSW_STATUS_returns(libfswatch.fsw_set_allow_overflow)
+FSW.set_callback = function(handle, callback)
+    local status = libfswatch.fsw_set_callback(handle, wrap_lua_callback(callback), nil)
+    return FSW_STATUS_to_lua_returns(status)
+end
+FSW.set_latency = wrap_FSW_STATUS_returns(libfswatch.fsw_set_latency)
+FSW.set_recursive = wrap_FSW_STATUS_returns(libfswatch.fsw_set_recursive)
+FSW.set_directory_only = wrap_FSW_STATUS_returns(libfswatch.fsw_set_directory_only)
+FSW.set_follow_symlinks = wrap_FSW_STATUS_returns(libfswatch.fsw_set_follow_symlinks)
+FSW.add_event_type_filter = function(session, ...)
+    local event_type_filter = box_parameters_as_type(fsw_event_type_filter, ...)
+    local status = libfswatch.fsw_add_event_type_filter(session, event_type_filter)
+    return FSW_STATUS_to_lua_returns(status)
+end
+FSW.add_filter = function(session, ...)
+    local cmonitor_filter = box_parameters_as_type(fsw_cmonitor_filter, ...)
+    local status = libfswatch.fsw_add_filter(session, cmonitor_filter)
+    return FSW_STATUS_to_lua_returns(status)
+end
+FSW.start_monitor = wrap_FSW_STATUS_returns(libfswatch.fsw_start_monitor)
+FSW.stop_monitor = wrap_FSW_STATUS_returns(libfswatch.fsw_stop_monitor)
+FSW.is_running = libfswatch.fsw_is_running
+FSW.destroy_session = wrap_FSW_STATUS_returns(libfswatch.fsw_destroy_session)
+FSW.last_error = libfswatch.fsw_last_error
+FSW.is_verbose = libfswatch.fsw_is_verbose
+FSW.set_verbose = libfswatch.fsw_set_verbose
 
 local methods = {
     add_path = FSW.add_path,
